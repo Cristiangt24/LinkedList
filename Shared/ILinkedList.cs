7a7bbfd55510e366b66168119f22d366574bd00b
Christiangt24 <cristiangutilds@hotmail.com>
2026-04-26 16:17:13 -0500

    Double Linked list process

---
+++ Shared/ILinkedList.cs
@@ -0,0 +1,13 @@
+﻿namespace Shared;
+
+public interface ILinkedList<T>
+{
+        bool Contains(T data);
+    void InsertAtBeginning(T data);
+    void InsertAtEnding(T data);
+    void InsertOrdered(T data);
+    void OrderList();
+    void Remove(T data);
+    void Reverse();
+    string ToString();
+}
